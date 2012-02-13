d953e5180f12045305c8b3d8ae46bbb224ba5fe0
Niko Schmuck <niko@nava.de>
2012-02-13 15:03:07 +0100

    Nesting of elements is now working.
    --HG--
    rename : public/stylesheets/main.css => public/stylesheets/cmskern.css

--- public/javascripts/widgets.js
+++ public/javascripts/widgets.js
@@ -17,33 +17,43 @@ angular.widget('my:form', function(element) {
 
         // process every field as specified in the JSON schema definition
         angular.forEach(schema, function processField(field) {
-            var fullyQualifiedName = this.parentName + '.' + field.name,
+            var qualifiedName = this.parentName + '.' + field.name,
+                fullyQualifiedName = this.fqName + '.' + field.name,
                 fieldElStr;
-            console.log("----> field: " + field.name);
+            console.log("----> field: " + qualifiedName);
 
             // has hierarchical subforms?
             if (field.children) {
-                console.log("      ----> (schema) children: " + fullyQualifiedName);
-                var contentChilds = scope.$eval(fullyQualifiedName);
+                var contentChilds = scope.$eval(qualifiedName);  // TODO: wird das noch benötigt?
                 var childElem = field.name + 'Elem';
-                var subfieldset = angular.element('<fieldset ng:repeat="' + childElem + ' in ' + fullyQualifiedName + '"></fieldset>');  // TODO: auf dieser Ebene ng:repeat fuer kinder
+                console.log("      ----> fieldset ng:repeat=" + childElem + " in " + qualifiedName);
+                // Nesting of ng:repeat must use relative variable reference names
+                var subfieldset = angular.element('<fieldset ng:repeat="' + childElem + ' in ' + qualifiedName + '"></fieldset>');
                 var legend = angular.element('<legend>' + field.label + '</legend>');
                 // ~~ add
-                var addButton = angular.element('<a href="#" ng:click="' + fullyQualifiedName + '.$add()"><i class="icon-plus" title="Add"></i></a>');
+                var addButton = angular.element('<a href="#" ng:click="' + qualifiedName + '.$add()"><i class="icon-plus" title="Add"></i></a>');
                 legend.append(addButton);
                 // ~~ remove
-                var removeButton = angular.element('<a href="#" ng:click="' + fullyQualifiedName + '.$remove(' + childElem + ')"><i class="icon-minus" title="Remove"></i></a>');
+                var removeButton = angular.element('<a href="#" ng:click="' + qualifiedName + '.$remove(' + childElem + ')"><i class="icon-minus" title="Remove"></i></a>');
                 legend.append(removeButton);
                 subfieldset.append(legend);
                 // ~~
-                angular.forEach(field.children, processField, {parentName: childElem, curDOMParent: subfieldset});
-                fieldset.append(subfieldset);
+                this.curDOMParent.append(subfieldset);
+                angular.forEach(field.children, processField, {parentName: childElem, fqName: fullyQualifiedName, curDOMParent: subfieldset});
+                console.log("~~ after add children   -> " + qualifiedName);
+                if (qualifiedName == 'teasersElem.childteasers') {
+                    console.log("childteasers finished: " + this.curDOMParent);
+                    // this.curDOMParent.append(fieldset);
+                } else if (qualifiedName == 'contentNode.teasers') {
+                    console.log("teasers finished: " + this.curDOMParent);
+                    // this.curDOMParent.append(fieldset);
+                }
                 return;
             }
 
             switch (field.type || 'text') {
                 case 'reference': {
-                    fieldElStr = '<input disabled name="' + fullyQualifiedName + '" ';
+                    fieldElStr = '<input disabled name="' + qualifiedName + '" ';
 
                     angular.forEach(field, function(value, attribute) {
                         if (attribute != 'tag') {
@@ -58,7 +68,7 @@ angular.widget('my:form', function(element) {
                 case 'checkbox':; //fallthrough
                 case 'password':; //fallthrough
                 case 'text': {
-                    fieldElStr = '<input name="' + fullyQualifiedName + '" ';
+                    fieldElStr = '<input name="' + qualifiedName + '" ';
 
                     angular.forEach(field, function(value, attribute) {
                         if (attribute != 'tag') {
@@ -70,7 +80,7 @@ angular.widget('my:form', function(element) {
                     break;
                 }
                 case 'textarea': {
-                    fieldElStr = '<textarea name="' + fullyQualifiedName + '" ';
+                    fieldElStr = '<textarea name="' + qualifiedName + '" ';
 
                     angular.forEach(field, function(attribute) {
                         fieldElStr += attribute + '="' + field[attribute] + '" ';
@@ -83,13 +93,15 @@ angular.widget('my:form', function(element) {
 
             var controlGroup = angular.element('<div class="control-group"></div>');
 
-            controlGroup.append(angular.element('<label class="control-label" for="' + fullyQualifiedName + '">' + field.label + '</label>'));
+            controlGroup.append(angular.element('<label class="control-label" for="' + qualifiedName + '">' + field.label + '</label>'));
             var controlElem = angular.element('<div class="controls">');
             controlElem.append(fieldElStr);
             controlGroup.append(controlElem);
+            console.log("****** append to " + qualifiedName);
             this.curDOMParent.append(controlGroup);
 
-        }, {parentName: data, curDOMParent: fieldset});
+        }, {parentName: data, fqName: data, curDOMParent: fieldset});
+
         angular.compile(fieldset)(scope);
         element.append(fieldset);
     };
