bf8de61a893f96e7557f16e865a9fbb3685a89a6
Niko Schmuck <niko@nava.de>
2012-07-06 13:34:31 +0200

    Weitere Anmerkungen zur Verschiebungsproblematik

--- playapp/public/javascripts/angular/addons/angular-widgets.js
+++ playapp/public/javascripts/angular/addons/angular-widgets.js
@@ -665,12 +665,15 @@ var widgetUtils = {
         return this.parseExpr(attr);
     },
     setValue: function (scope, attrExpr, value){
+        console.log("000");
         if(!attrExpr || !attrExpr.expression)
             return;
         var v = value;
         v = this.parseValue(v, attrExpr, scope);
         scope.$set(attrExpr.expression, v);
+        console.log("111");
         scope.$parent.$eval();
+        console.log("222");
     },
     getValue: function (scope, attrExpr){
         if(!attrExpr || !attrExpr.expression)
@@ -992,16 +995,27 @@ angular.directive('ui:sortable', function(expression, templateElement, val) {
                 var items = scope.$get(itemsExpr.expression);
                 console.log("**** BEFORE: " + dump(items, 1));
                 items.splice(end, 0, items.splice(start, 1)[0]);
-                console.log("****   AFTER: " + dump(items, 1));
+                console.log("****    AFTER: " + dump(items, 1));
+                scope.$set(itemsExpr.expression, items);
+                scope.$parent.$eval();  // HIER DURCH WIRD REIHENFOLGE IM MODELL RICHTIG EINGESTELLT, ABER im UI wieder verstellt
+                // QUASI gleichbedeutend: widgetUtils.setValue(scope, itemsExpr, items); // FIXME: hier im eval wird Reihenfolge wieder zurückgesetzt
+
+                // Weitere Lösungsansätze:
+                //      (1) DOM Order manipulieren
+                //      (2) Array Manipulation mehr in einem angularJS Stil?
+
                 // ~~ TODO: how to solve the timing issues for syncing changed model to update view?
                 //setTimeout(function() {
                 //    scope.$eval();
                 //}, 100);
                 // ~~~ DID ALSO not HELP scope.$updateView();
                 // include changing the index as part of 'ng:repeat-index' attribute
-                scope.$eval();
-                console.log("****        AFTER EVAL: " + dump(items, 1));
-                //scope.$updateViews(); // TODO: makes no difference...
+
+                //scope.$tryEval(itemsExpr.expression, templateElement);
+                // KLAPPT NICHT: e.stopPropagation();
+
+//                console.log("****        AFTER EVAL: " + dump(items, 1));
+                //scope.$updateView(); // TODO: makes no difference...
             }
         });
     }
